69e7b5e0312008101b3fa8f73011d68544f3a596
KatanES <businesskatan.es@gmail.com>
2023-08-03 21:59:17 +0200

    1st task commented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,22 @@
-import { QuizList } from './quizList';
-import quizItems from '../components/data.json';
-import { SearchBar } from './SearchBar';
+// import { QuizList } from './quizList';
+// import quizItems from '../components/data.json';
+// import { SearchBar } from './SearchBar';
 
-import { Profile } from './ProfileCard';
-import user from '../components/user.json';
+// import { Profile } from './ProfileCard';
+// import user from '../components/user.json';
 
-export const App = () => {
-  return (
-    <>
-      {/* <SearchBar />
-      <QuizList items={quizItems} /> */}
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </>
-  );
-};
+// export const App = () => {
+//   return (
+//     <>
+//       {/* <SearchBar />
+//       <QuizList items={quizItems} /> */}
+//       <Profile
+//         username={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+//       />
+//     </>
+//   );
+// };
